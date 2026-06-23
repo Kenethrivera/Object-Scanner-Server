9dca09e6d2b473b057d19dc38bb9d660ceccdcbc
Keneth Rivera <riverakenethjames@gmail.com>
2026-06-23 19:01:15 +0800

    fix port

--- main.py
+++ main.py
@@ -58,5 +58,6 @@ async def analyze_frame(image: UploadFile = File(...)):
     
 if __name__ == "__main__":
     import uvicorn
+    import os
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
